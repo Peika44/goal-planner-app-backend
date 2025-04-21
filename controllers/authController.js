af6dbe9a11204147de57f8e468ec477fbacad033
Peika44 <p3xu@ucsd.edu>
2025-04-21 09:17:11 -0700

    for test

--- controllers/authController.js
+++ controllers/authController.js
@@ -28,9 +28,10 @@ exports.login = async (req, res) => {
     if (!match) return res.status(400).json({ msg: "Invalid credentials" });
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-    console.log("New user created:", newUser);
+    console.log("User logged in:", user);
     res.json({ token });
   } catch (err) {
-    res.status(500).json({ msg: "Error in login" });
+    console.error("Login error:", err); 
+    res.status(500).json({ msg: "Server error" });
   }
 };
--- routes/goals.js
+++ routes/goals.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const { createGoal, getUserGoals } = require("../controllers/goalController");
-// const path = require('path');
-// const authenticateToken = require(path.resolve(__dirname, '../middleware/auth'));
 const authenticateToken = require("../middleware/auth");
 
 router.post("/", authenticateToken, createGoal);
